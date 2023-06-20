09094eba64ea6f61d3cf18fd9a44644bbf02dacb
atchakal <ahmed.tchakal3@docaposte.fr>
2023-06-20 12:37:07 +0200

    Implement login form with auth module : Add login componenet

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
   {
-    path: 'auth',
-    loadChildren: () => import('./auth.module').then(m => m.AuthModule)
-  }
+    path: 'login',
+    component: LoginComponent
+  },
 ];
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import {ReactiveFormsModule} from "@angular/forms";
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule,
