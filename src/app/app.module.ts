2aeacb2c3aa488818800260aa4a8d37d87270ffb
atchakal <ahmed.tchakal3@docaposte.fr>
2023-06-20 12:17:26 +0200

    Implement login form with auth module : Add auth.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModile } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent
@@ -11,7 +11,7 @@ import { HttpClientModile } from '@angular/common/http'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModile
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
